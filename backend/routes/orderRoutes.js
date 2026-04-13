453c451b7e7646c3aeaf29b1e5e0c86ab2421a2f
Gaurav Katiyar <183537767+Gaurav1-coder@users.noreply.github.com>
2026-04-13 08:10:18 +0530

    [Gaurav] added order routes

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { addOrder, getOrders, updateOrder, deleteOrder } = require("../controllers/orderController");
+
+router.post("/", addOrder);
+router.get("/", getOrders);
+router.put("/:id", updateOrder);
+router.delete("/:id", deleteOrder);
+
+module.exports = router;
