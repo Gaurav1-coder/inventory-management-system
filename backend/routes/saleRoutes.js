f04b2c91af177309544bb193de8b9e7c594b9c83
Gaurav Katiyar <183537767+Gaurav1-coder@users.noreply.github.com>
2026-04-13 23:28:43 +0530

    [Gaurav] added category controller.

--- backend/routes/saleRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { addSale, getSales } = require("../controllers/saleController");
-const { protect } = require("../middleware/authMiddleware");
-const router = express.Router();
-
-router.post("/", protect, addSale);
-router.get("/", protect, getSales);
-
-module.exports = router;
