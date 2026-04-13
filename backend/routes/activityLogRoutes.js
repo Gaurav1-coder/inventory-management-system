f15e49cc1df7cd949444817e7c66771bb892a5be
Vami112 <solankiriddhi68@gmail.com>
2026-04-14 01:01:32 +0530

    [VAMIKA]updated routers

--- backend/routes/activityLogRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getActivityLogs } = require("../controllers/activityLogController");
-const { protect, adminGuard } = require("../middleware/authMiddleware");
-const router = express.Router();
-
-router.get("/", protect, adminGuard, getActivityLogs);
-
-module.exports = router;
--- backend/routes/stockTransactionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { addStockTransaction, getStockTransactions } = require("../controllers/stockTransactionController");
-const { protect } = require("../middleware/authMiddleware");
-const router = express.Router();
-
-router.post("/", protect, addStockTransaction);
-router.get("/", protect, getStockTransactions);
-
-module.exports = router;
