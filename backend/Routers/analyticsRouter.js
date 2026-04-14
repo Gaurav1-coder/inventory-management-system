fb623e3ba2f0854fdf65ec4ddeb4ff3e5e083fef
Gaurav Katiyar <183537767+Gaurav1-coder@users.noreply.github.com>
2026-04-15 00:03:52 +0530

    remove backend folder

--- backend/Routers/analyticsRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getDashboardStats } = require("../controller/analyticsController");
-const { authmiddleware } = require("../middleware/Authmiddleware");
-
-router.get("/dashboard-stats", authmiddleware, getDashboardStats);
-
-module.exports = router;
