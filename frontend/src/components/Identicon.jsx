1bbe826421e7487672370cc834a71402438bd2e8
Gaurav Katiyar <183537767+Gaurav1-coder@users.noreply.github.com>
2026-04-15 15:01:24 +0530

    merge pull remove forntend

--- frontend/src/components/Identicon.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { toSvg } from "jdenticon";
-
-const Identicon = ({ value, size }) => {
-  const svgString = toSvg(value, size);
-  return <div dangerouslySetInnerHTML={{ __html: svgString }} />;
-};
-
-export default Identicon;
